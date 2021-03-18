8e54d20728fcc50b0b11d738895dc387bd4f6b4c
IT Admin <you@example.com>
2021-03-18 18:52:05 +0330

    ~

--- server3/train.py
+++ server3/train.py
@@ -1,8 +1,13 @@
+import os
+import glob
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from sklearn import svm, calibration
 import pickle
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(os.path.dirname(PATH), 'data')
 
 
 def train_dosing():
@@ -33,7 +38,15 @@ def train_holder():
     name = 'holder'
     FPR = 400.0
     CPR = 100.0
-    X = np.load('/home/it/Desktop/data_holder/frames_%s.npy' % name)
+    files = glob.glob(DATA_PATH + '/frames_%s_*.npy' % name)
+    X = [np.load(file) for file in files]
+    for x in X:
+        print(x.shape)
+        assert len(x.shape[0]) == len(X[0].shape[0])
+        assert len(x.shape[0]) % FPR == 0
+    X = np.concatenate(X)
+    print(X.shape)
+
     y = np.arange(X.shape[0]) / (FPR / CPR) - 0.5
     y = np.mod(y.round().astype(int) + int(CPR / 2), CPR) - int(CPR / 2)
     train(CPR, X, y, name)
@@ -52,7 +65,7 @@ def train(C, X, y, name):
     X_test = X_test[IX, :]
     y_test = y_test[IX]
 
-    clf = svm.LinearSVC(C=1, max_iter=10, verbose=True)
+    clf = svm.LinearSVC(C=1, max_iter=20, verbose=True)
     clf = calibration.CalibratedClassifierCV(clf)
     clf = clf.fit(X_train, y_train)
 
@@ -75,5 +88,5 @@ def train(C, X, y, name):
     # plt.show()
 
 
-# train_holder()
-train_dosing()
+train_holder()
+# train_dosing()
