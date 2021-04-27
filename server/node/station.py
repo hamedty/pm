fc6ec88599e2b2d95df8d45ac547c47785246393
IT Admin <you@example.com>
2021-04-27 16:17:56 +0430

    ~

--- server/node/station.py
+++ server/node/station.py
@@ -34,7 +34,7 @@ class Station(Node):
                 'pin_pulse': 35,
                 'pin_dir': 33,
                 'pin_limit_n': 28,
-                'course': 50000,
+                'course': 12000,
                 'homing_delay': 200,
                 'home_retract': 100,
                 'microstep': 2500,
--- server/script-station.py
+++ server/script-station.py
@@ -11,17 +11,29 @@ async def main():
     # await NODE.send_command_reset_arduino()
     await NODE.send_command_config_arduino()
 
-    # await N
-    # await NODE.send_command({'verb': 'set_valves', 'valves': [0, 0], })
+    # Homing
+    # await NODE.send_command({'verb': 'set_valves', 'valves': [0, 0, 0, 0, 0, 0], })
     # await asyncio.sleep(.25)
+    await NODE.send_command({'verb': 'home', 'axis': 3, })
+    # while True:
+    #     await NODE.send_command({'verb': 'set_valves', 'valves': [1, 1, 0, 1, 1, 0], })
+    #     await asyncio.sleep(1)
+    #     await NODE.send_command({'verb': 'set_valves', 'valves': [0, 0, 0, 0, 0, 0], })
+    #     await asyncio.sleep(1)
 
-    # await NODE.send_command({'verb': 'home', 'axis': 1, })
-    # await NODE.send_command({'verb': 'home', 'axis': 0, })
     # await asyncio.sleep(1)
     await NODE.send_command({'verb': 'move_motors', 'moves': [
-        [000, 250, 0],
-        [1000, 250, 0]
+        [10000, 250, 0],
+        [10000, 250, 0],
+        [10000, 250, 0],
+        [000, 250, 1],
     ]})
+    # await NODE.send_command({'verb': 'move_motors', 'moves': [
+    #     [000, 250, 0],
+    #     [000, 250, 0],
+    #     [000, 250, 0],
+    #     [-10000, 250, 1],
+    # ]})
 
 asyncio.run(main())
 # set valves 0
@@ -30,3 +42,4 @@ asyncio.run(main())
 # 4k up (4.5)
 # fwd 5k + 10k + 5+5 -3 + 1 -.5 - .3
 # close jacks (1,1)
+# 11
