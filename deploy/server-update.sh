61dad503c98207590049e03b067c02c9ee6d4abc
IT Admin <you@example.com>
2021-05-26 20:00:42 +0430

    everything works

--- deploy/server-update.sh
+++ deploy/server-update.sh
@@ -1,8 +1,8 @@
-for i in  100 #101 # 102
+for i in  100 101 # 102
 do
   echo 192.168.44.$i
   scp -rq /home/it/Desktop/PAM2060/server/ pi@192.168.44.$i:~/
   # scp -r /home/it/Desktop/PAM2060/models/holder_$i.clf pi@192.168.44.$i:~/models/holder.clf
   # scp -r /home/it/Desktop/PAM2060/models/dosing_$i.clf pi@192.168.44.$i:~/models/dosing.clf
-  # ssh pi@192.168.44.$i "sudo systemctl restart rpi.service"
+  ssh pi@192.168.44.$i "sudo systemctl restart rpi.service"
 done
