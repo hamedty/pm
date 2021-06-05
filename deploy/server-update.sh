5372fb8f59be53fb8607ed9daf9d6e0dc35eaaa1
IT Admin <you@example.com>
2021-06-05 17:08:15 +0430

    ~

--- deploy/server-update.sh
+++ deploy/server-update.sh
@@ -1,8 +1,15 @@
-for i in  101 102 103 104 105
+# for i in  101 102 103 104 105
+# do
+#   echo 192.168.44.$i
+#   # scp -rq /home/it/Desktop/PAM2060/server/ pi@192.168.44.$i:~/
+#   rsync -avz --progress   /home/it/Desktop/PAM2060/models/holder_$i.clf pi@192.168.44.$i:~/models/holder.clf
+#   rsync -avz --progress   /home/it/Desktop/PAM2060/models/dosing_$i.clf pi@192.168.44.$i:~/models/dosing.clf
+#   ssh pi@192.168.44.$i "sudo systemctl restart rpi.service"
+# done
+
+for i in  100
 do
   echo 192.168.44.$i
-  # scp -rq /home/it/Desktop/PAM2060/server/ pi@192.168.44.$i:~/
-  # scp -r /home/it/Desktop/PAM2060/models/holder_$i.clf pi@192.168.44.$i:~/models/holder.clf
-  # scp -r /home/it/Desktop/PAM2060/models/dosing_$i.clf pi@192.168.44.$i:~/models/dosing.clf
+  scp -rq /home/it/Desktop/PAM2060/server/ pi@192.168.44.$i:~/
   ssh pi@192.168.44.$i "sudo systemctl restart rpi.service"
 done
