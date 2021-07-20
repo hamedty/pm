db4658ef96a4d4807e9c96d3744805c08d19f7ec
IT Admin <you@example.com>
2021-07-20 11:57:52 +0430

    ~

--- server/node/feeder.py
+++ server/node/feeder.py
@@ -7,38 +7,38 @@ class Feeder(Node):
     arduino_reset_pin = 21
 
     g2core_config_base = [
-        # # X - Rail Motor
-        (3, {
+        # X - Cartridge Motor 1
+        (1, {
             'ma': 0,  # map to X
             'sa': 1.8,  # step angle 1.8
-            'tr': 5,  # travel per rev = 5mm
+            'tr': 360,  # travel per rev = 5mm
             'mi': 4,  # microstep = 4
             'po': 1,  # direction
         }),
         ('x', {
             'am': 1,  # standard axis mode
-            'vm': 20000,  # max speed
-            'fr': 800000,  # max feed rate
+            'vm': 360 * 1000,  # max speed
+            'fr': 360 * 1000,  # max feed rate
             'tn': 0,  # min travel
-            'tm': 719,  # max travel
-            'jm': 20000,  # max jerk
-            'jh': 8000,  # hominzg jerk
+            'tm': 200,  # max travel
+            'jm': 90000,  # max jerk
+            'jh': 90000,  # hominzg jerk
             'hi': 1,  # home switch
             'hd': 0,  # homing direction
-            'sv': 1000,  # home search speed
-            'lv': 200,  # latch speed
+            'sv': 5000,  # home search speed
+            'lv': 5000,  # latch speed
             'zb': 1,  # zero backoff
         }),
-        ('di1mo', 0),  # Homing Switch - Mode = Active Low - NO
+        ('di1mo', 1),  # Homing Switch - Mode = Active High - NC
         ('di1ac', 1),
         ('di1fn', 1),
 
-        # Y - Cartridge Motor 1
+        # Y - Cartridge Motor 2
         (2, {
             'ma': 1,  # map to Y
             'sa': 1.8,  # step angle 1.8
             'tr': 360,  # travel per rev = 5mm
-            'mi': 2,  # microstep = 2
+            'mi': 4,  # microstep = 4
             'po': 1,  # direction
         }),
         ('y', {
@@ -47,43 +47,43 @@ class Feeder(Node):
             'fr': 360 * 1000,  # max feed rate
             'tn': 0,  # min travel
             'tm': 200,  # max travel
-            'jm': 100000,  # max jerk
-            'jh': 100000,  # hominzg jerk
+            'jm': 90000,  # max jerk
+            'jh': 90000,  # hominzg jerk
             'hi': 2,  # home switch
             'hd': 0,  # homing direction
-            'sv': 360 * 5,  # home search speed
-            'lv': 360,  # latch speed
+            'sv': 5000,  # home search speed
+            'lv': 5000,  # latch speed
             'zb': 1,  # zero backoff
         }),
         ('di2mo', 1),  # Homing Switch - Mode = Active High - NC
         ('di2ac', 1),
         ('di2fn', 1),
 
-        # Z - Cartridge Motor 2
-        ('di3mo', 1),  # Homing Switch - Mode = Active High - NC
-        ('di3ac', 1),
-        ('di3fn', 1),
-        (1, {
+        # Z - Rail Motor
+        (3, {
             'ma': 2,  # map to Z
             'sa': 1.8,  # step angle 1.8
-            'tr': 360,  # travel per rev = 5mm
-            'mi': 4,  # microstep = 2
+            'tr': 5,  # travel per rev = 5mm
+            'mi': 4,  # microstep = 4
             'po': 1,  # direction
         }),
         ('z', {
             'am': 1,  # standard axis mode
-            'vm': 360 * 1000,  # max speed
-            'fr': 360 * 1000,  # max feed rate
+            'vm': 20000,  # max speed
+            'fr': 800000,  # max feed rate
             'tn': 0,  # min travel
-            'tm': 200,  # max travel
-            'jm': 90 * 1000,  # max jerk
-            'jh': 90000,  # hominzg jerk
+            'tm': 719,  # max travel
+            'jm': 20000,  # max jerk
+            'jh': 8000,  # hominzg jerk
             'hi': 3,  # home switch
             'hd': 0,  # homing direction
-            'sv': 5000,  # home search speed
-            'lv': 5000,  # latch speed
+            'sv': 1000,  # home search speed
+            'lv': 200,  # latch speed
             'zb': 1,  # zero backoff
         }),
+        ('di3mo', 0),  # Homing Switch - Mode = Active Low - NO
+        ('di3ac', 1),
+        ('di3fn', 1),
 
 
         ('jt', 1.00),
@@ -112,10 +112,10 @@ class Feeder(Node):
             'vacuum 2': 14,
         },
         'di': {
-            'x-': 1,
+            'z-': 1,
         },
         'encoders': {
-            'posx': ['enc1', 480.0, 1.0],  # encoder key, ratio, telorance
+            'posz': ['enc1', 480.0, 1.0],  # encoder key, ratio, telorance
         }
 
     }
@@ -124,14 +124,14 @@ class Feeder(Node):
         await self.send_command_raw('!\n\x04', wait_start=[], wait_completion=False)
         await asyncio.sleep(1)
 
-        # rail
-        await self.send_command_raw('G28.2 X0')
-
         # cartridge pickers
-        # await self.send_command_raw('G28.2 Y0')
+        await self.send_command_raw('G28.2 X0')
+        await self.send_command_raw('G28.2 Y0')
+
+        # rail
         await self.send_command_raw('G28.2 Z0')
 
         # # reset encoder
         await self.send_command_raw('G28.5')
-        await self.send_command_raw('G1 X1 F1000')
-        await self.send_command_raw('G1 X0 F1000')
+        await self.send_command_raw('G1 X16 F1000')
+        await self.send_command_raw('G1 X90 Y90 F10000')
--- server/scripts/test_feeder.py
+++ server/scripts/test_feeder.py
@@ -7,10 +7,64 @@ async def main(system, ALL_NODES):
     feeder = await gather_feeder(system, ALL_NODES)
     print('feeder ready')
 
-    # await feeder.set_valves([0] * 14)
+    await feeder.set_valves([0] * 14)
     # await asyncio.sleep(.5)
-    # await feeder.home()
+    await feeder.home()
+
+    # await feeder.set_valves([None, 1])
     #
+    # t0 = time.time()
+    # for i in range(11):
+    #     print('hi')
+    #     # await feeder.send_command_raw(
+    #     #     # bring finger down (7) and open sub-gate(4)
+    #     #     'M100 ({out7: 1, out4: 0}) \n'
+    #     #     'G4 P.2 \n'
+    #     # )
+    #     # await feeder.send_command_raw(
+    #     #     # open main gate
+    #     #     'M100 ({out3: 1}) \n'
+    #     #     'G4 P.2 \n'
+    #     # )
+    #     # await feeder.send_command_raw(
+    #     #     # push forward
+    #     #     'M100 ({out6: 1}) \n'
+    #     #     # 'G4 P.6 \n'
+    #     # )
+    #     # await asyncio.sleep(.5)
+    #     # await feeder.send_command_raw(
+    #     #     # close main gate again and close sub-gate
+    #     #     'M100 ({out3: 0, out4: 1}) \n'
+    #     #     # 'G4 P.2 \n'
+    #     # )
+    #     # await feeder.send_command_raw(
+    #     #     # bring up finger
+    #     #     'M100 ({out7: 0}) \n'
+    #     #     # 'G4 P.2 \n'
+    #     # )
+    #
+    #     x = 16 + 25 * (i + 1)
+    #     c = (
+    #         # bring pusher back
+    #         # 'M100 ({out6: 0}) \n'
+    #         # 'G4 P1.5 \n'
+    #         'G1 X%d F6000\n' % x
+    #     )
+    #
+    #     print(c)
+    #     await feeder.send_command_raw(c)
+    #     # await feeder.send_command_raw('G1 X%d F6000' % x)
+    #     print('-------------------', i)
+    #     # await asyncio.sleep(1)
+    # print(time.time() - t0)
+    # # await feeder.send_command_raw('G1 X719 F6000')
+    # await feeder.G1(x=719, feed=6000)
+    # await feeder.send_command_raw('M100 ({out2: 0}) \n')
+    # await feeder.G1(x=16, feed=6000)
+    # # await feeder.send_command_raw('G1 X16 F6000')
+    # print(time.time() - t0)
+    # print('bye')
+    # # cartridge
     # for i in range(10):
     #     await feeder.send_command_raw(
     #         'G1 Z180 F100000 \n'
@@ -26,6 +80,32 @@ async def main(system, ALL_NODES):
     #         'M100 ({out9: 1}) \n'
     #         'G4 P.2 \n'
     #     )
+
+    # holder
+    # await asyncio.sleep(3)
+    # for i in range(1):
+    #     await feeder.send_command_raw(
+    #         # bring finger down (7) and open sub-gate(4)
+    #         'M100 ({out7: 1, out4: 0}) \n'
+    #         'G4 P.2 \n'
+    #         # open main gate
+    #         'M100 ({out3: 1}) \n'
+    #         'G4 P.2 \n'
+    #         # push forward
+    #         'M100 ({out6: 1}) \n'
+    #         'G4 P.6 \n'
+    #         # close main gate again and close sub-gate
+    #         'M100 ({out3: 0, out4: 1}) \n'
+    #         'G4 P.2 \n'
+    #         # bring up finger
+    #         'M100 ({out7: 0}) \n'
+    #         'G4 P.2 \n'
+    #         # bring pusher back
+    #         'M100 ({out6: 0}) \n'
+    #         'G4 P1.5 \n'
+    #     )
+    #
+    # await feeder.set_valves([0] * 14)
 
     # while True:
     #     await feeder.home()
