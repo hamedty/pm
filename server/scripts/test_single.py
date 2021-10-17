6d17798073bcddc02d21307931d874daeca08667
IT Admin <you@example.com>
2021-10-17 13:12:36 +0330

    ~

--- server/scripts/test_single.py
+++ server/scripts/test_single.py
@@ -11,37 +11,6 @@ from node import ALL_NODES_DICT
 async def main(system, ALL_NODES):
     all_nodes, feeder, rail, robots, stations = await gather_all_nodes(system, ALL_NODES, wait_for_readiness=False)
 
-    # stations = stations[4:]
-    # H_ALIGNING H_PUSH H_PRE_DANCE
-    # for station in stations:
-    #     await station.G1(z=station.hw_config['H_ALIGNING'], feed=5000)
-    #     await asyncio.sleep(1)
-
-    # v = 0
-    # while True:
-    #     v = 1 - v
-    #     await asyncio.gather(*[station.set_valves([0, 0, v]) for station in stations])
-    #     await asyncio.sleep(2)
-
-    # # await feeder.home()
-    # t0 = time.time()
-    # z = 16
-    # feed = 50000
-    # # for i in range(10):
-    # #     z += 25
-    # #     await feeder.G1(z=z, feed=feed)
-    # command = ''
-    # for i in range(10):
-    #     z += 25
-    #     command += 'G1Z%dF%d\n' % (z, feed)
-    #
-    # await feeder.send_command_raw(command)
-    #
-    # node = ALL_NODES_DICT['Dosing Feeder 1']
-    # for i in range(50):
-    #     await node.set_valves([i % 2])
-    #     await asyncio.sleep(0.8)
-
     dosing_feeder = ALL_NODES_DICT['Dosing Feeder 1']
     holder_feeder = ALL_NODES_DICT['Feeder 1']
 
@@ -49,20 +18,12 @@ async def main(system, ALL_NODES):
     await holder_feeder.set_motors((5, 25), (9, 5))
     # await holder_feeder.set_valves([None] * 10 + [1])
     try:
-        # for i in range(100):
         while True:
-
-            # set channel selector 1= channel 1 open
-            # await dosing_feeder.set_valves([0, None, 1])
-
             # wait for optic sensor input=1
             await dosing_feeder.wait_metric('in1')
 
             # wait for buffer to be free
             await dosing_feeder.wait_metric('in3', 0)
-
-            # close gate
-            await dosing_feeder.set_valves([None, None, 0])
 
             # wait for proximity_input value established
             await asyncio.sleep(.2)
