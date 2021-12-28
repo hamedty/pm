aad8300afc5eca39b702a30956bf20a850e3bdc6
IT Admin <you@example.com>
2021-12-28 18:34:46 +0330

    ~

--- deploy/fw-upload.sh
+++ deploy/fw-upload.sh
@@ -1,15 +1,15 @@
-# # Robots & Rail
-# for i in 100
-# do
-#   echo 192.168.44.$i
-#   scp -rq /home/it/Desktop/g2/g2core/bin/pm-robot/g2core.bin  pi@192.168.44.$i:~/firmware-robot.bin
-#   scp -rq /home/it/Desktop/g2/g2core/bin/pm-rail/g2core.bin  pi@192.168.44.$i:~/firmware-rail.bin
-#   ssh pi@192.168.44.$i "sudo systemctl stop rpi.service"
-#   ssh pi@192.168.44.$i './arduino-cli upload -p `readlink -f /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0` --fqbn arduino:sam:arduino_due_x --input-file firmware-robot.bin'
-#   ssh pi@192.168.44.$i './arduino-cli upload -p `readlink -f /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0` --fqbn arduino:sam:arduino_due_x --input-file firmware-robot.bin'
-#   ssh pi@192.168.44.$i './arduino-cli upload -p `readlink -f /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0` --fqbn arduino:sam:arduino_due_x --input-file firmware-rail.bin'
-#   ssh pi@192.168.44.$i "sudo systemctl restart rpi.service"
-# done
+# Robots & Rail
+for i in 100
+do
+  echo 192.168.44.$i
+  scp -rq /home/it/Desktop/g2/g2core/bin/pm-robot/g2core.bin  pi@192.168.44.$i:~/firmware-robot.bin
+  scp -rq /home/it/Desktop/g2/g2core/bin/pm-rail/g2core.bin  pi@192.168.44.$i:~/firmware-rail.bin
+  ssh pi@192.168.44.$i "sudo systemctl stop rpi.service"
+  ssh pi@192.168.44.$i './arduino-cli upload -p `readlink -f /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0` --fqbn arduino:sam:arduino_due_x --input-file firmware-robot.bin'
+  ssh pi@192.168.44.$i './arduino-cli upload -p `readlink -f /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0` --fqbn arduino:sam:arduino_due_x --input-file firmware-robot.bin'
+  ssh pi@192.168.44.$i './arduino-cli upload -p `readlink -f /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0` --fqbn arduino:sam:arduino_due_x --input-file firmware-rail.bin'
+  ssh pi@192.168.44.$i "sudo systemctl restart rpi.service"
+done
 
 
 # Stations
--- server/scripts/test_single.py
+++ server/scripts/test_single.py
@@ -11,4 +11,8 @@ from node import ALL_NODES_DICT
 async def main(system, ALL_NODES):
     all_nodes, feeder, dosing_feeder, rail, robots, stations = await gather_all_nodes(system, ALL_NODES, wait_for_readiness=False)
     s = stations[0]
+
+    await s.home()
+    await s.set_eac(eac1=50)
     await s.send_command_raw("G1Z100F30000")
+    await s.set_eac(eac1=500)
